3aa6359e7996a8f1e521eb97db35b7f2032eb11d
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2021-09-10 21:37:14 +0800

    重寫拉哈哈

--- js/navbar.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-    /* menu */
-    $(".show-btn").on("click", function () {
-        $(this).parent().toggleClass("show");
-    });
-    $(".menu .blurry").on("click", function () {
-        $(this).parent().removeClass("show");
-    })
-});
---
+++ src/components/navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div></div>
+  );
+};
+
+export default Navbar;
