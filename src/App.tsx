e471d496e711a9c942b129c93a037d6484780e66
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2021-09-10 22:36:38 +0800

    home

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import Navbar from './components/navbar';
+import Home from './components/home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 
 const App = () => {
   return (
-    <div className="App">
+    <Router>
       <Navbar />
-    </div>
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
   );
 };
 
--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
+import Logo from './img/logo-white.a9c53c85.svg';
+import './sass/home.sass';
 
 const Home = () => {
   return (
-    <div className="home"></div>
+    <div className="home">
+      <img src={Logo} />
+      <p>台南高工 網頁設計社</p>
+      <div className="icons">
+        <a className="fab fa-github" href="https://github.com/NTIHS-IT"></a>
+        <a className="fab fa-facebook" href="https://www.facebook.com/103730941540184"></a>
+        <a className="fab fa-instagram" href="https://www.instagram.com/ntihs.web/"></a>
+        <a className="fab fa-youtube" href="https://www.youtube.com/channel/UCex1cjm-Z6WGP1O1CSNTVpw"></a>
+      </div>
+    </div>
   );
 };
 
