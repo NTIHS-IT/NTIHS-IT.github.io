a6b79d2ca85f332431605ecbb58810c7a94c08d3
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2021-09-10 22:13:03 +0800

    add navbar

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div></div>
+    <div className="home"></div>
   );
 };
 
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,8 +1,35 @@
 import React from 'react';
+import Logo from './img/logo-white.a9c53c85.svg';
+import './sass/navbar.sass';
 
 const Navbar = () => {
   return (
-    <div></div>
+    <ul className="navbar">
+      <li className="logo">
+        <a href="/">
+          <img src={Logo} />
+          <h1>NTIHS</h1>
+        </a>
+      </li>
+      <li>
+        <a href="./classList.html" target="_blank">
+          課表
+        </a>
+      </li>
+      <li>
+        <a href="https://ntihs-it.github.io/NTIHS-Blog/" target="_blank" rel="noreferrer">
+          Blog
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://peing.net/en/ntihs112th"
+          target="_blank" rel="noreferrer"
+        >
+          匿名
+        </a>
+      </li>
+    </ul>
   );
 };
 
