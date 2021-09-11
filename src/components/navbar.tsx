9933a1b0c062e89597e797966e9df7726ba2029c
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2021-09-11 11:10:05 +0800

    add RWD

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,36 +1,47 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import Logo from './img/logo-white.a9c53c85.svg';
 import './sass/navbar.sass';
 
 const Navbar = () => {
+  const [menuSwitch, setMenuSwitch] = useState(false);
+
   return (
-    <ul className="navbar">
-      <li className="logo">
+    <div className="navbar">
+      <div className="logo">
         <Link to="/">
           <img src={Logo} />
           <h1>NTIHS</h1>
         </Link>
-      </li>
-      <li>
-        <Link to="/classList">
-          課表
-        </Link>
-      </li>
-      <li>
-        <a href="https://ntihs-it.github.io/NTIHS-Blog/" target="_blank" rel="noreferrer">
-          Blog
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://peing.net/en/ntihs112th"
-          target="_blank" rel="noreferrer"
-        >
-          匿名
-        </a>
-      </li>
-    </ul>
+      </div>
+      <ul className={menuSwitch ? 'open-menu menu' : 'menu'}>
+        <li>
+          <Link to="/classList">
+            課表
+          </Link>
+        </li>
+        <li>
+          <a href="https://ntihs-it.github.io/NTIHS-Blog/" target="_blank" rel="noreferrer">
+            Blog
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://peing.net/en/ntihs112th"
+            target="_blank" rel="noreferrer"
+          >
+            匿名
+          </a>
+        </li>
+      </ul>
+      <div className="menu-button">
+        <input type="checkbox" checked={menuSwitch} onClick={() => {
+          setMenuSwitch(!menuSwitch);
+          console.log(menuSwitch);
+        }}/>
+        <div></div>
+      </div>
+    </div>
   );
 };
 
