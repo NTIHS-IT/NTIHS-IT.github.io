b078619a51ff5b5baef1d90282812542067d0138
Young-TW <young20050727@gmail.com>
2021-09-14 19:49:56 +0800

    各校聯絡資訊

--- src/components/data/cooperate.ts
+++ src/components/data/cooperate.ts
@@ -1,7 +1,16 @@
 import scist from './img/scist.png';
 import itrc from './img/itrc.jpg';
+import kspgc from './img/kspgc.jpg';
+import tngs from './img/tngs.jpg';
+import tfcis from './img/tfcis.jpg';
+import hkhs from './img/hkhs.jpg';
+import ccsh from './img/ccsh.jpg';
+import tnssh from './img/tnssh.jpg';
+import cygsh from './img/cygsh.jpg';
+import cysh from './img/cysh.jpg';
 import fscs from './img/fscs.jpg';
 import fgisc from './img/fgisc.jpg';
+import infor from './img/infor.jpg';
 import hgisc from './img/hgisc.jpg';
 import HCCR from './img/HCCR.jpg';
 import FHCRC from './img/FHCRC.jpg';
@@ -22,6 +31,49 @@ const data = [
     'fb': 'https://www.facebook.com/KGHSI2TRC2/',
   },
   {
+    'name': '雄中程設',
+    'ig': 'https://www.instagram.com/kspgc_112th/',
+    'avatar': kspgc,
+  },
+  {
+    'name': '南女資研',
+    'ig': 'https://www.instagram.com/tngs_tgirc/',
+    'avatar': tngs,
+    'fb': 'https://www.facebook.com/tngsirc',
+  },
+  {
+    'name': '南一中資訊',
+    'ig': 'https://www.instagram.com/tfcis_at_tnfsh/',
+    'avatar': tfcis,
+    'website': 'https://www.tfcis.org',
+  },
+  {
+    'name': '興國資訊',
+    'ig': 'https://www.instagram.com/hkhsirc_5th/',
+    'avatar': hkhs,
+  },
+  {
+    'name': '家齊資研',
+    'ig': 'https://www.instagram.com/ccsh_ccirs_6th/',
+    'avatar': ccsh,
+    'fb': 'https://www.facebook.com/CCIRS.since2016/',
+  },
+  {
+    'name': '南二中資研',
+    'ig': '(https://www.instagram.com/tnssh.ciss_112/',
+    'avatar': tnssh,
+  },
+  {
+    'name': '嘉女資研',
+    'ig': 'https://www.instagram.com/cygsh_irc_8/',
+    'avatar': cygsh,
+  },
+  {
+    'name': '嘉中資研',
+    'ig': 'https://www.instagram.com/cysh_irc/',
+    'avatar': cysh,
+  },
+  {
     'name': '鳳中電資',
     'ig': 'https://www.instagram.com/fssh.cs.club/',
     'avatar': fscs,
@@ -33,6 +85,12 @@ const data = [
     'avatar': fgisc,
     'fb': 'https://www.facebook.com/scist.tw',
     'blog': 'https://scist.org/blog',
+  },
+  {
+    'name': '建中資訊',
+    'ig': 'https://www.instagram.com/infor.34th/',
+    'avatar': infor,
+    'fb': 'https://www.facebook.com/infor.34th/',
   },
   {
     'name': '竹女資研',
