0ecc62e81772d2f8b9e5b067b0dd11f7d9e2956f
Young-TW <young20050727@gmail.com>
2021-09-15 19:29:23 +0800

    增加合作頁面人物

--- src/components/cooperates.tsx
+++ src/components/cooperates.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import cooperates from './data/cooperate';
+import cooperatesPeople from './data/cooperatePeople';
 import Tilt from 'react-parallax-tilt';
 import Links from './links/links';
 import './sass/cooperates.sass';
@@ -7,6 +8,9 @@ import './sass/cooperates.sass';
 const Cooperates = () => {
   return (
     <div className="cooperates">
+      <div className='title'>
+        {'組織'}
+      </div>
       {cooperates.map((cooperate) => {
         return (
           <Tilt
@@ -24,6 +28,31 @@ const Cooperates = () => {
                 ig={cooperate.ig}
                 fb={cooperate.fb}
                 blog={cooperate.blog}
+              />
+            </div>
+          </Tilt>
+        );
+      })}
+      <div className='title'>
+        {'人物'}
+      </div>
+      {cooperatesPeople.map((cooperatePeople) => {
+        return (
+          <Tilt
+            key={cooperatePeople.name}
+            className="cooperate"
+            glareEnable={true}
+            glareMaxOpacity={1}
+            glareBorderRadius={'6px'}
+          >
+            <img src={cooperatePeople.avatar} className="cooperate-avatar"/>
+            <div className="content">
+              <h2>{cooperatePeople.name}</h2>
+              <Links
+                website={cooperatePeople.website}
+                ig={cooperatePeople.ig}
+                fb={cooperatePeople.fb}
+                blog={cooperatePeople.blog}
               />
             </div>
           </Tilt>
---
+++ src/components/data/cooperatePeople.ts
@@ -0,0 +1,23 @@
+import kane from './img/kane.jpg';
+import monkey from './img/monkey.png';
+
+const data = [
+  {
+    'name': '凱恩',
+    'ig': 'https://www.instagram.com/kane_tw/',
+    'avatar': kane,
+    'website': 'https://iskane.me',
+    // 'fb': ,
+    // 'blog': ,
+  },
+  {
+    'name': '猴子',
+    // 'ig': '',
+    'avatar': monkey,
+    'website': 'https://a3510377.github.io',
+    'fb': 'https://www.facebook.com/profile.php?id=100028122045493',
+    'blog': 'https://a3510377.github.io',
+  },
+];
+
+export default data;
